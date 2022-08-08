b0f89bfcd702d5c7761123a0db208e7dc59fd112
John Sweeney <jes2806@gmail.com>
2022-08-08 13:54:46 -0600

    adding chages to repo

--- express_server.js
+++ express_server.js
@@ -19,6 +19,10 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("*", (req, res) => {
+  res.status(404).send("This is not the page you are looking for");
+}); // added this because I learned it in lecture and would like to test it!
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
