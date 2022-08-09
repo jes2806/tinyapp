750486ddfcc0e21ebb91324222a94359f26603ef
John Sweeney <jes2806@gmail.com>
2022-08-08 20:53:19 -0600

    adding to repo

--- express_server.js
+++ express_server.js
@@ -40,9 +40,16 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get(`/u/:id`, (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const id = generateRandomString();
+  console.log(req.body, req.params); // Log the POST request body to the console
+  urlDatabase[id] = req.body.longURL;
+  res.redirect(`urls/${id}`);
 });
 
 app.get("*", (req, res) => {
