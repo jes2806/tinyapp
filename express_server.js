ab6821fa30381c675f369bc0620bb6efb3f047d4
John Sweeney <jes2806@gmail.com>
2022-08-09 18:26:01 -0600

    pushing after implementing edit

--- express_server.js
+++ express_server.js
@@ -29,6 +29,13 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls/:id", (req, res) => {
+  const shortID = req.params.id;
+  const longURL = req.body.longURL;
+  urlDatabase[shortID] = longURL;
+  res.redirect("/urls");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
