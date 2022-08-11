35e9a583f73f99d9a06487f36007c9a618cf3e70
John Sweeney <jes2806@gmail.com>
2022-08-11 16:23:40 -0600

    commiting my new changes with user log in

--- express_server.js
+++ express_server.js
@@ -22,6 +22,16 @@ const passwordCheck = function(password, users) {
   }
 };
 
+const urlsForUser = function(id) {
+  const userURL = {};
+  for (const key in urlDatabase) {
+    if (urlDatabase[key].userID === id) {
+      userURL[key] = urlDatabase[key];
+    };
+  }
+  return userURL;
+};
+
 app.set('view engine', 'ejs');
 
 const users = {
@@ -38,8 +48,14 @@ const users = {
 };
 
 const urlDatabase = {
-  'b2xVn2': 'http://www.lighthouselabs.ca',
-  '9sm5xK': 'http://www.google.com'
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
 };
 
 app.use(express.urlencoded({ extended: true }));
@@ -47,28 +63,77 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.post('/urls/:key/delete', (req, res) => {
-  console.log('delete route key:', req.params.key);
+  const shortIDs = Object.keys(urlsForUser(req.cookies.user_id))
+  if (!urlDatabase[req.params.key]) {
+    res.send('You have entered an invalid id!');
+    return;
+  }
+  if (!req.cookies.user_id) {
+    res.send('You must be logged in to do this!');
+    return;
+  }
+  if (!shortIDs.includes(req.params.key)) {
+    res.send('You do not have access to this shortned URL, please access your urls: <a href="/urls">here</a>')
+    return;
+  }
   delete urlDatabase[req.params.key];
+  res.redirect('/urls');
+});
+
+app.post('/urls/:id', (req, res) => {
+  const shortID = req.params.id;
+  const longURL = req.body.longURL;
+  const shortIDs = Object.keys(urlsForUser(req.cookies.user_id))
+  if (!shortIDs.includes(req.params.id)) {
+    res.send('You do not have access to this shortned URL, please access your urls: <a href="/urls">here</a>')
+    return;
+  }
+  if (!urlDatabase[req.params.id]) {
+    res.send('You have entered an invalid id!');
+    return;
+  }
+  if (!req.cookies.user_id) {
+    res.send('You must be logged in to do this!');
+    return;
+  }
+  urlDatabase[shortID] = { longURL, userID: req.cookies['user_id'] };
   res.redirect('/urls');
 });
 
 app.get('/urls', (req, res) => {
   const id = req.cookies['user_id']
-  const templateVars = { urls: urlDatabase, user: users[id] };
-  res.render('urls_index', templateVars);
+  if (!id) {
+    res.send('You must login in order to view shortened URLS: <br> <a href= "/login">Login</a> <br><a href= "/register">Register</a>');
+    return;
+  } else {
+    const templateVars = { urls: urlsForUser(id), user: users[id] };
+    res.render('urls_index', templateVars);
+  }
 });
 
 app.get('/urls/new', (req, res) => {
+  if (!req.cookies.user_id) {
+    res.redirect('/login')
+    return;
+  }
   const templateVars = { urls: urlDatabase, user: users[req.cookies['user_id']] };
   res.render('urls_new', templateVars);
 });
 
 app.get('/register', (req, res) => {
+  if (req.cookies.user_id) {
+    res.redirect('/urls')
+    return;
+  }
   const templateVars = { urls: urlDatabase, user: users[req.cookies['user_id']] };
   res.render('register', templateVars);
 });
 
 app.get('/login', (req, res) => {
+  if (req.cookies.user_id) {
+    res.redirect('/urls')
+    return;
+  }
   const templateVars = { urls: urlDatabase, user: users[req.cookies['user_id']] };
   res.render('login', templateVars);
 });
@@ -97,15 +162,18 @@ app.post('/logout', (req, res) => {
   res.redirect('/urls');
 });
 
-app.post('/urls/:id', (req, res) => {
-  const shortID = req.params.id;
-  const longURL = req.body.longURL;
-  urlDatabase[shortID] = longURL;
-  res.redirect('/urls');
-});
 
 app.get('/urls/:id', (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], user: users[req.cookies["user_id"]] };
+  if (!req.cookies.user_id) {
+    res.send('You must be <a href= "/login"> Logged in </a> in order to view this page.')
+    return;
+  }
+  const shortIDs = Object.keys(urlsForUser(req.cookies.user_id))
+  if (!shortIDs.includes(req.params.id)) {
+    res.send('You do not have access to this shortned URL, please access your urls: <a href="/urls">here</a>')
+    return;
+  }
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id].longURL, user: users[req.cookies["user_id"]] };
   res.render('urls_show', templateVars);
 });
 
@@ -115,13 +183,20 @@ app.get('/urls.json', (req, res) => {
 
 app.get('/u/:id', (req, res) => {
   const longURL = urlDatabase[req.params.id];
-  res.redirect(longURL);
+  if (!longURL) {
+    return res.status(404).send('You have entered an invalid shortened URL');
+  }
+  res.redirect(longURL.longURL);
 });
 
 app.post('/urls', (req, res) => {
+  if (!req.cookies.user_id) {
+    res.send('You must login in order to shorten URLS');
+    return;
+  }
   const id = generateRandomString();
   console.log(req.body, req.params); // Log the POST request body to the console
-  urlDatabase[id] = req.body.longURL;
+  urlDatabase[id] = { longURL: req.body.longURL, userID: req.cookies.user_id }
   res.redirect(`urls/${id}`);
 });
 
@@ -145,9 +220,9 @@ app.post('/register', (req, res) => {
   res.redirect('/urls');
 });
 
-app.get('*', (req, res) => {
+app.get('*', (req, res) => { // added this because I learned it in lecture and would like to test it!
   res.status(404).send('This is not the page you are looking for');
-}); // added this because I learned it in lecture and would like to test it!
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
